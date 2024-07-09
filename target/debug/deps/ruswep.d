49fba52c50219e7885cbfd127a78cfe76a4f128b
unknown <orpheas.dax@gmail.com>
2024-07-09 13:34:06 +0300

    commit post cargo build

---
+++ target/debug/deps/ruswep.d
@@ -0,0 +1,5 @@
+E:\TheRest\GitPortableSetup\ruswem\target\debug\deps\ruswep.exe: src/main.rs
+
+E:\TheRest\GitPortableSetup\ruswem\target\debug\deps\ruswep.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/ruswep.d
@@ -0,0 +1,1 @@
+E:\TheRest\GitPortableSetup\ruswem\target\debug\ruswep.exe: E:\TheRest\GitPortableSetup\ruswem\src\main.rs
